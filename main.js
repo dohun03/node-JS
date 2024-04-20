eed74a245276fb4b96e09ad35e64bf9ea7aa4582
dohun03 <167541796+dohun03@users.noreply.github.com>
2024-04-20 23:55:08 +0900

    node js 게시판 코드 수정
    삭제 기능 및 express 모듈 사용, 작성자 IP 표시

--- main.js
+++ main.js
@@ -1,32 +1,28 @@
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
-var qs = require('querystring');
-var express = require('express');
-var path = require('path');
-var page = require('./lib/page.js');
-// var template = require('./lib/create.js');
-var mysql = require('mysql');
-var db = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '0000',
-  database : 'nodejs'
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+const qs = require('querystring');
+const page = require('./lib/page.js');
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '0000',
+  database: 'nodejs'
 });
 
 db.connect();
 
-var app = http.createServer(function(request,response){
-    var _url = request.url;
-    var queryData = url.parse(_url, true).query;
-    var pathname = url.parse(_url, true).pathname;
-
-    if(pathname === '/'){
-      db.query(`SELECT * FROM tbl_board;`, function(error, data){
-        html = '';
-        for(let i=0; i<data.length; i++){
-          html +=
-          `
+app.get('/', (req, res) => {
+  db.query(`SELECT * FROM tbl_board;`, function(error, data) {
+    if (error) {
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+    let html = '';
+    for (let i = 0; i < data.length; i++) {
+      html +=
+        `
           <div class="accordion-item">
             <h2 class="accordion-header">
               <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${i}" aria-expanded="false" aria-controls="collapse${i}">
@@ -42,40 +38,49 @@ var app = http.createServer(function(request,response){
             </div>
           </div>
           `;
-        }
-        html = `<div class="accordion" id="accordionExample">${html}</div>`;
-        response.writeHead(200);
-        response.end(page.main(html));
-      });
+    }
+    html = `<div class="accordion" id="accordionExample">${html}</div>`;
+    res.send(page.main(html));
+  });
+});
 
-    } else if(pathname === '/view') {
-      db.query(`SELECT * FROM tbl_board where id=${queryData.id}`, function(error, data){
-        html = `
-        <h3 style="background-color:white; padding:5px; width:100%; margin-bottom:10px; font-weight:bold;">${data[0].subject}</h3>
-        <div style="background-color:white; padding:5px; width:100%; margin-bottom:10px;">${data[0].content}</div>
-        <br>
-        <button class="btn btn-sm btn-success edit">Edit</button>
-        <button class="btn btn-sm btn-danger delete">Delete</button>
-        `;
-        script = `
-        let edit = document.querySelector(".edit");
-        edit.addEventListener("click", function() {
-          document.location.href = '/edit?id=${queryData.id}';
-        });
-        `
-        response.writeHead(200);
-        response.end(page.main(html, script));
-      });
-    } else if(pathname === '/edit'){
-      if(queryData.id){
-        db.query(`SELECT * FROM tbl_board where id=${queryData.id}`, function(error, data){
-          html = `
+app.get('/view', (req, res) => {
+  const id = req.query.id;
+  db.query(`SELECT * FROM tbl_board where id=${id}`, function(error, data) {
+    if (error) {
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+    if (data && data.length > 0) {
+      const html = `
+            <h3 style="background-color:white; padding:10px; width:100%; margin-bottom:10px; font-weight:bold;">${data[0].subject}</h3>
+            <div>작성자 IP: ${data[0].user}</div>
+            <div style="background-color:white; padding:10px; width:100%; margin-bottom:10px; white-space: pre-line;">${data[0].content}</div>
+            <br>
+            <a href="/delete?id=${id}"><button class="btn btn-sm btn-danger delete">Delete</button></a>
+          `;
+      res.send(page.main(html));
+    } else {
+      res.status(404).send("요청하신 게시글을 찾을 수 없습니노.");
+    }
+  });
+});
+
+app.get('/edit', (req, res) => {
+  const id = req.query.id;
+  if (id) {
+    db.query(`SELECT * FROM tbl_board where id=${id}`, function(error, data) {
+      if (error) {
+        res.status(500).send('Internal Server Error');
+        return;
+      }
+      const html = `
           <input class="form-control subject" value="${data[0].subject}" placeholder="제목을 입력하세요" style="margin-bottom:10px;">
           <textarea class="form-control content" style="height:80%;">${data[0].content}</textarea>
           <br>
           <button class="btn btn-sm btn-primary save">Save</button>
-          `
-          script = `
+          `;
+      const script = `
           $(document).ready(function() {
             $('.save').click(function(e) {
               e.preventDefault();
@@ -84,10 +89,9 @@ var app = http.createServer(function(request,response){
               var content = $('.content').val();
               
               $.ajax({
-                url: '/create',
+                url: '/write',
                 method: 'POST',
                 data: { 
-                  // send: 1,
                   status: 'update',
                   subject: subject, 
                   content: content
@@ -97,87 +101,93 @@ var app = http.createServer(function(request,response){
                   document.location.href = '/';
                 },
                 error: function(xhr, status, error) {
-                  console.log(xhr, status, error);
+                  console.log('fuck');
                 }
               });
             });
           });
-          `
-          response.writeHead(200);
-          response.end(page.main(html, script));
-        });
-      } else {
-        html = `
+          `;
+      res.send(page.main(html, script));
+    });
+  } else {
+    let user = req.ip;
+    user = user.replace("::ffff:","");
+    const html = `
         <input class="form-control subject" placeholder="제목을 입력하세요" style="margin-bottom:10px;">
         <textarea class="form-control content" style="height:80%;"></textarea>
         <br>
         <button class="btn btn-sm btn-primary save">Save</button>
-        `
-        script = `
+        `;
+    const script = `
         $(document).ready(function() {
           $('.save').click(function(e) {
-            e.preventDefault();
-            
-            var subject = $('.subject').val();
-            var content = $('.content').val();
-            
-            $.ajax({
-              url: '/create',
-              method: 'POST',
-              data: { 
-                // send: 1,
-                status: 'create',
-                subject: subject, 
-                content: content
-               },
-              success: function(response) {
-                console.log('suc');
-                document.location.href = '/';
-              },
-              error: function(xhr, status, error) {
-                console.log(xhr, status, error);
-              }
-            });
+            if ($('.subject').val().length < 30) {
+              e.preventDefault();
+              
+              var subject = $('.subject').val();
+              var content = $('.content').val();
+              
+              $.ajax({
+                url: '/write',
+                method: 'POST',
+                data: { 
+                  status: 'create',
+                  subject: subject, 
+                  content: content,
+                  user: '${user}'
+                },
+                success: function(response) {
+                  console.log('suc');
+                  document.location.href = '/';
+                },
+                error: function(xhr, status, error) {
+                  console.log(xhr, status, error);
+                }
+              });
+            } else {
+              alert("글자는 20자 이내로 쳐 쓰세요");
+            }
           });
         });
-        `
-        response.writeHead(200);
-        response.end(page.main(html, script));
-      }
-    } else if(pathname === '/create'){
-      var body = '';
-      request.on('data', function(data){
-          body = body + data;
-      });
-      request.on('end', function(){
-        var post = qs.parse(body);
-        if(post.status == 'create'){
-          db.query(`INSERT INTO tbl_board (subject, content) VALUES (?,?);`,[post.subject, post.content], function(error, data){
-            if(error)
-              throw error;
-  
-            response.writeHead(200);
-            response.end();
-          });
-        } else if(post.status == 'update'){
-          // db.query(`update tbl_board (subject, content) VALUES (?,?);`,[post.subject, post.content], function(error, data){
-          //   if(error)
-          //     throw error;
-  
-          //   response.writeHead(200);
-          //   response.end();
-          // });
-        } else {
-          response.writeHead(404);
-          response.end('Not found, Go home');
-        }
-      });
-    } else if(pathname === '/delete'){
-      response.writeHead(404);
-      response.end('Not found');
-    } else {
-      response.writeHead(404);
-      response.end('Not found');
-    }
+        `;
+    res.send(page.main(html, script));
+  }
 });
-app.listen(3000);
+
+app.post('/write', (req, res) => {
+  let body = '';
+  req.on('data', function(data) {
+    body = body + data;
+  });
+  req.on('end', function() {
+    const post = qs.parse(body);
+    if (post.status == 'create') {
+      db.query(`INSERT INTO tbl_board (subject, content, user) VALUES (?,?,?);`, [post.subject, post.content, post.user], function(error, data) {
+        if (error) {
+          res.status(500).send('Internal Server Error');
+          return;
+        }
+        res.send();
+      });
+    } else if (post.status == 'update') {
+      // Update logic here
+    } else {
+      res.status(404).send('Not found, Go home');
+    }
+  });
+});
+
+app.get('/delete', (req, res) => {
+  const id = req.query.id;
+  db.query(`DELETE FROM tbl_board where id=${id}`, function(error, data) {
+    if (error) {
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+    res.redirect('/');
+  });
+});
+
+app.listen(3000, () => {
+  console.log('서버가 3000번 포트에서 실행 중입니다.');
+});
