28602716349c1cb2e9d2fbb20aa628eaeeded015
dohun03 <167541796+dohun03@users.noreply.github.com>
2024-05-08 21:43:22 +0900

    코드의 문제점과 개선사항
    보안 문제:
    현재 비밀번호가 코드에 하드코딩되어 있습니다. 이는 보안 취약점으로 이어질 수 있습니다. 비밀번호를 코드에 직접 넣는 대신 환경 변수나 별도의 설정 파일을 통해 비밀번호를 관리하는 것이 좋습니다.
    세션 시크릿 값이 코드에 하드코딩되어 있습니다. 이 값을 외부에서 접근할 수 없도록 보안적으로 안전한 곳에 저장하고, 코드에는 환경 변수나 설정 파일을 통해 해당 값을 참조하는 것이 좋습니다.
    
    비동기 처리 문제:
    페이지 개수를 계산하는 쿼리와 게시글을 조회하는 쿼리가 비동기적으로 실행되고 있지 않습니다. 페이지 개수를 계산하는 쿼리가 완료되기 전에 페이지를 렌더링하는 문제가 있을 수 있습니다. 이를 해결하기 위해 Promise나 async/await를 사용하여 쿼리를 순차적으로 실행하거나, 콜백 함수를 사용하여 쿼리 간 의존성을 관리할 수 있습니다.
    
    에러 처리 부재:
    쿼리 실행 중 발생할 수 있는 에러에 대한 적절한 처리가 부족합니다. 에러 핸들링을 통해 사용자에게 적절한 오류 메시지를 보여주고, 서버의 안정성을 유지할 수 있습니다.
    
    중복 코드:
    HTML 코드 중복이 발생하고 있습니다. 특히, 수정 페이지와 회원가입 페이지에서 HTML 코드가 거의 동일합니다. 이러한 중복을 줄이고 유지보수를 쉽게 하기 위해 템플릿 엔진을 사용하거나 공통 레이아웃을 별도로 분리하는 것이 좋습니다.
    
    스크립트 관리 부재:
    페이지 스크립트가 페이지 본문과 섞여 있습니다. 이는 유지보수를 어렵게 만들 수 있습니다. 스크립트를 별도의 파일로 분리하고, 필요한 경우 외부 라이브러리를 사용하여 관리하는 것이 좋습니다.
    
    코드 구조의 미흡:
    코드가 단일 파일에 모두 존재하고 있어 유지보수가 어려울 수 있습니다. 코드를 기능별로 모듈화하고 적절한 디렉토리 구조를 사용하여 코드를 구성하는 것이 좋습니다.

--- main.js
+++ main.js
@@ -2,7 +2,19 @@ const express = require('express');
 const app = express();
 const mysql = require('mysql');
 const qs = require('querystring');
-const page = require('./lib/page.js');
+const pages = require('./lib/pages.js');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const FileStore = require('session-file-store')(session)
+
+app.use(cookieParser());
+app.use(express.static(__dirname));
+app.use(session({
+  secret: 'dhlee', // 따로 보관해야함. 나만 알아야함.
+  resave: false,
+  saveUninitialized: true,
+  store: new FileStore()
+}))
 
 const db = mysql.createConnection({
   host: 'localhost',
@@ -14,11 +26,20 @@ const db = mysql.createConnection({
 db.connect();
 
 app.get('/', (req, res) => {
+  console.log(req.session);
+  let page = req.query.page ? parseInt(req.query.page):1;
   let line = 5;
-  if(req.query.line){
-    line = parseInt(req.query.line);
+  if(req.cookies.line){
+    line = parseInt(req.cookies.line);
   }
-  db.query(`SELECT * FROM tbl_board limit ?;`, [line],function(error, data) {
+
+  //페이지 개수 합계
+  let page_total;
+  db.query(`SELECT COUNT(*) FROM tbl_board`,function(error, data) {
+    page_total = Math.ceil(parseInt(data[0]['COUNT(*)']) / line);
+  });
+
+  db.query(`SELECT * FROM tbl_board order by id desc limit ?,?;`, [line*(page-1), line],function(error, data) {
     if (error) {
       res.status(500).send('Internal Server Error');
       console.log(error)
@@ -39,17 +60,22 @@ app.get('/', (req, res) => {
         `;
     }
     let selectOptionsHtml = '';
-    const options = [5, 10, 50, 100]; // 선택할 수 있는 옵션들
+    const options = [5, 10, 50, 100];
     options.forEach(option => {
-      const selected = option === line ? 'selected' : ''; // 현재 line 값과 option 값이 같은 경우 selected 설정
+      const selected = option === line ? 'selected' : '';
       selectOptionsHtml += `<option value="${option}" ${selected}>${option} 줄</option>`;
     });
     html = `
       <div class="info" style="width:100%; margin-bottom: 10px;">
-        <a href="/edit"><button type="button" class="btn btn-dark">글쓰기</button></a>
+        <a href="/edit"><button type="button" class="btn btn-dark"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> 글쓰기</button></a>
         <select class="form-select form-select-sm line" aria-label="Small select example" style="display:inline; width:100px;">
-        ${selectOptionsHtml}
+          ${selectOptionsHtml}
         </select>
+        <div class="float-end">
+          <span style="margin-right:10px;">${page} / ${page_total}</span>
+          <a href="${page==1?'#':'/?page='+(page-1).toString()}"><button type="button" class="btn btn-dark" ${page==1?'disabled':''}><i class="fa fa-arrow-left" aria-hidden="true"></i></button></a>
+          <a href="${page_total==page?'#':'/?page='+(page+1).toString()}"><button type="button" class="btn btn-dark" ${page_total==page ? 'disabled':''}><i class="fa fa-arrow-right" aria-hidden="true"></i></button></a>
+        </div>
       </div>
       <table class="table table-hover" style="border: 1px solid #ccc;">
         <col style="width:80%;"><col><col style="width:7%;">
@@ -64,18 +90,45 @@ app.get('/', (req, res) => {
         ${html}
         </tbody>
       </table>
+      <div style="width:100%; background-color:white; padding:15px; border:5px solid black; border-radius:15px;">
+        <h3>2024.04.23 패치노트</h3>
+        <ul>
+          <li>DHOUTSIDE 로고 추가(수제작)</li>
+          <li>줄(line) 설정 기능 추가</li>
+          <li>페이지 넘기기 기능 추가</li>
+          <li>수정 및 삭제는 작성자만 가능하도록 변경</li>
+          <li>로그인/회원가입 기능 / 제작 중</li>
+          <li>댓글 기능 / 제작 중</li>
+        </ul>
+      </div>
     `
     script = `
     $(".line").on("change", function() {
-      document.location.href = '/?line='+$(this).val();
+      let line = $(this).val();
+      $.ajax({
+        url: '/cookie',
+        method: 'POST',
+        data: {
+          line: line
+         },
+        success: function(response) {
+          console.log('suc');
+          document.location.href = '/';
+        },
+        error: function(xhr, status, error) {
+          console.log('error');
+        }
+      });
     })
     `
-    res.send(page.main(html,script));
+    res.send(pages.main(html,script));
   });
 });
 
 app.get('/view', (req, res) => {
   const id = req.query.id;
+  let user = req.ip;
+  user = user.replace("::ffff:","");
   db.query(`SELECT * FROM tbl_board where id=${id}`, function(error, data) {
     if (error) {
       res.status(500).send('Internal Server Error');
@@ -83,14 +136,19 @@ app.get('/view', (req, res) => {
     }
     if (data && data.length > 0) {
       const html = `
-            <h3 style="background-color:white; padding:10px; width:100%; margin-bottom:10px; font-weight:bold;">${data[0].subject}</h3>
-            <div style="padding:10px;">작성자 IP: ${data[0].user}</div>
-            <div style="background-color:white; padding:10px; width:100%; margin-bottom:10px; white-space: pre-line;">${data[0].content}</div>
-            <br>
-            <a href="/edit?id=${id}"><button class="btn btn-sm btn-success edit">Edit</button></a>
-            <a href="/delete?id=${id}"><button class="btn btn-sm btn-danger delete">Delete</button></a>
-          `;
-      res.send(page.main(html));
+        <h3 style="background-color:white; padding:10px; width:100%; margin-bottom:10px; font-weight:bold;">${data[0].subject}</h3>
+        <div style="padding:10px;">작성자 IP: ${data[0].user}</div>
+        <div style="background-color:white; padding:10px; width:100%; margin-bottom:10px; white-space: pre-line;">${data[0].content}</div>
+        <br>
+        <a href="/edit?id=${id}" class="${data[0].user!=user?'none':''}"><button class="btn btn-sm btn-success edit">Edit</button></a>
+        <a href="/delete?id=${id}" class="${data[0].user!=user?'none':''}"><button class="btn btn-sm btn-danger delete">Delete</button></a>
+        <hr>
+        <div class="comment" style="margin-top:20px;">
+          <textarea class="form-control content" placeholder="댓글을 입력하세요"></textarea><br>
+          <a href="#"><button class="btn btn-sm btn-primary">댓글 저장</button></a>
+        </div>
+      `;
+      res.send(pages.main(html));
     } else {
       res.status(404).send("요청하신 게시글을 찾을 수 없습니다람쥐썬더");
     }
@@ -107,43 +165,47 @@ app.get('/edit', (req, res) => {
       }
       let user = req.ip;
       user = user.replace("::ffff:","");
-      const html = `
-          <input class="form-control subject" value="${data[0].subject}" placeholder="제목을 입력하세요" style="margin-bottom:10px;">
-          <textarea class="form-control content" style="height:80%;">${data[0].content}</textarea>
-          <br>
-          <button class="btn btn-sm btn-primary save">Save</button>
-          `;
-      const script = `
-          $(document).ready(function() {
-            $('.save').click(function(e) {
-              
-              e.preventDefault();
-              
-              var subject = $('.subject').val();
-              var content = $('.content').val();
-              
-              $.ajax({
-                url: '/write',
-                method: 'POST',
-                data: { 
-                  id: '${id}',
-                  status: 'update',
-                  subject: subject, 
-                  content: content,
-                  user: '${user}'
-                 },
-                success: function(response) {
-                  console.log('suc');
-                  document.location.href = '/view?id=${id}';
-                },
-                error: function(xhr, status, error) {
-                  console.log('error');
-                }
-              });
-            });
+      let html = `
+      <input class="form-control subject" value="${data[0].subject}" placeholder="제목을 입력하세요" style="margin-bottom:10px;">
+      <textarea class="form-control content" style="height:80%;">${data[0].content}</textarea>
+      <br>
+      <button class="btn btn-sm btn-primary save">Save</button>
+      `;
+      let script = `
+      $(document).ready(function() {
+        $('.save').click(function(e) {
+          
+          e.preventDefault();
+          
+          var subject = $('.subject').val();
+          var content = $('.content').val();
+          
+          $.ajax({
+            url: '/write',
+            method: 'POST',
+            data: { 
+              id: '${id}',
+              status: 'update',
+              subject: subject, 
+              content: content,
+              user: '${user}'
+              },
+            success: function(response) {
+              console.log('suc');
+              document.location.href = '/view?id=${id}';
+            },
+            error: function(xhr, status, error) {
+              console.log('error');
+            }
           });
-          `;
-      res.send(page.main(html, script));
+        });
+      });
+      `;
+      if(data[0].user!=user) {
+        html = '<h3>당신은 수정 권한이 없습니다.</h3>'
+        script = '';
+      }
+      res.send(pages.main(html, script));
     });
   } else {
     let user = req.ip;
@@ -181,12 +243,12 @@ app.get('/edit', (req, res) => {
                 }
               });
             } else {
-              alert("글자는 20자 이내로 쳐 쓰세요");
+              alert("글자는 20자 이내로 쓰세요");
             }
           });
         });
         `;
-    res.send(page.main(html, script));
+    res.send(pages.main(html, script));
   }
 });
 
@@ -222,19 +284,157 @@ app.post('/write', (req, res) => {
 
 app.get('/delete', (req, res) => {
   const id = req.query.id;
-  db.query(`DELETE FROM tbl_board where id=?`, [id], function(error, data) {
-    if (error) {
-      res.status(500).send('Internal Server Error');
-      return;
+  db.query(`select user FROM tbl_board where id=?`, [id], function(error, data) {
+    let user = req.ip;
+    user = user.replace("::ffff:","");
+    if(data[0].user==user){
+      db.query(`DELETE FROM tbl_board where id=?`, [id], function(error, data) {
+        if (error) {
+          res.status(500).send('Internal Server Error');
+          return;
+        }
+        res.redirect('/');
+      });
+    } else {
+      res.send("<h3>당신은 삭제 권한이 없습니다.</h3>");
     }
-    res.redirect('/');
+  })
+});
+
+app.post('/cookie', (req, res) => {
+  let body = '';
+  req.on('data', function(data) {
+    body = body + data;
+  });
+  req.on('end', function() {
+    const post = qs.parse(body);
+    res.cookie('line', post.line, { maxAge: 86400000 }); // 1일 동안 쿠키 유지
+    res.send();
   });
 });
 
 app.get('/login', (req, res) => {
-  res.status(404).send('아직 구현중인 기능입니노');
-})
-
-app.listen(3000, () => {
-  console.log('서버가 3000번 포트에서 실행 중입니다.');
+  html = `
+    <style>
+        .container {
+            max-width: 500px;
+            margin: 100px auto;
+            background: #fff;
+            padding: 20px;
+            border-radius: 5px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+        h2 {
+            text-align: center;
+        }
+        .form-group {
+            margin-bottom: 20px;
+        }
+        label {
+            font-weight: bold;
+            display: block;
+            margin-bottom: 5px;
+        }
+        input[type="text"], input[type="password"] {
+            width: 100%;
+            padding: 10px;
+            border-radius: 5px;
+            border: 1px solid #ccc;
+        }
+        input[type="submit"] {
+            width: 100%;
+            background: #007bff;
+            color: #fff;
+            border: none;
+            padding: 10px;
+            border-radius: 5px;
+            cursor: pointer;
+        }
+        input[type="submit"]:hover {
+            background: #0056b3;
+        }
+    </style>
+    <div class="container">
+      <h2>로그인</h2>
+      <form action="/login_process" method="POST">
+          <div class="form-group">
+              <label for="username">사용자 아이디</label>
+              <input type="text" id="username" name="username" required>
+          </div>
+          <div class="form-group">
+              <label for="password">비밀번호</label>
+              <input type="password" id="password" name="password" required>
+          </div>
+          <input type="submit" value="로그인">
+      </form>
+      <br>
+      <a href="/signup">혹시 아이디가 없으신가요? 회원가입을 해라 당장.</a>
+    </div>
+  `
+  res.send(pages.main(html));
 });
+
+app.get('/signup', (req, res) => {
+  html = `
+    <style>
+        .container {
+            max-width: 500px;
+            background: #fff;
+            padding: 20px;
+            border-radius: 5px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+        h2,h3 {
+            text-align: center;
+        }
+        .form-group {
+            margin-bottom: 20px;
+        }
+        label {
+            font-weight: bold;
+            display: block;
+            margin-bottom: 5px;
+        }
+        input[type="text"], input[type="password"] {
+            width: 100%;
+            padding: 10px;
+            border-radius: 5px;
+            border: 1px solid #ccc;
+        }
+        input[type="submit"] {
+            width: 100%;
+            background: #007bff;
+            color: #fff;
+            border: none;
+            padding: 10px;
+            border-radius: 5px;
+            cursor: pointer;
+        }
+        input[type="submit"]:hover {
+            background: #0056b3;
+        }
+    </style>
+    <div class="container">
+      <h3>회원가입 전 서비스 이용 약관 동의</h3>
+      <div class="terms">
+          서비스에 액세스하거나 서비스를 사용함으로써 귀하는 본 약관을 준수할 것에 동의하게 됩니다.
+      </div>
+      <form action="/signup_process" method="POST">
+          <label for="agree"><input type="checkbox" id="agree" name="agree" required> 서비스<span style="text-decoration: line-through;">(노예)</span> 약관에 동의합니다.</label><br>
+          <div class="form-group">
+              <label for="username">사용자 아이디</label>
+              <input type="text" id="username" name="username" required>
+          </div>
+          <div class="form-group">
+              <label for="password">비밀번호</label>
+              <input type="password" id="password" name="password" required>
+          </div>
+          <input type="submit" value="회원가입">
+      </form>
+      <br>
+    </div>
+  `
+  res.send(pages.main(html));
+});
+
+app.listen(3000);
