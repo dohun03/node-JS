6efba7a2422e302e2f804f2f5aab0609184dfe4a
dohun03 <167541796+dohun03@users.noreply.github.com>
2024-05-16 16:27:26 +0900

    로그인 / 회원가입 기능 완성

--- main.js
+++ main.js
@@ -1,32 +1,31 @@
 const express = require('express');
 const app = express();
-const mysql = require('mysql');
 const qs = require('querystring');
 const pages = require('./lib/pages.js');
 const cookieParser = require('cookie-parser');
-const session = require('express-session');
-const FileStore = require('session-file-store')(session)
+const session = require('./lib/session.js');
+const db = require('./lib/db.js');
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(__dirname));
-app.use(session({
-  secret: 'dhlee', // 따로 보관해야함. 나만 알아야함.
-  resave: false,
-  saveUninitialized: true,
-  store: new FileStore()
-}))
+app.use(session);
 
-const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '0000',
-  database: 'nodejs'
-});
+const authData = {
+  username: 'swag',
+  password: '1111'
+}
 
-db.connect();
+function authStatus(req, res) {
+  if(req.session.is_logined)
+    return req.session.username;
+  else
+    return false;
+}
 
 app.get('/', (req, res) => {
-  console.log(req.session);
   let page = req.query.page ? parseInt(req.query.page):1;
   let line = 5;
   if(req.cookies.line){
@@ -42,7 +41,6 @@ app.get('/', (req, res) => {
   db.query(`SELECT * FROM tbl_board order by id desc limit ?,?;`, [line*(page-1), line],function(error, data) {
     if (error) {
       res.status(500).send('Internal Server Error');
-      console.log(error)
       return;
     }
     let html = '';
@@ -71,6 +69,10 @@ app.get('/', (req, res) => {
         <select class="form-select form-select-sm line" aria-label="Small select example" style="display:inline; width:100px;">
           ${selectOptionsHtml}
         </select>
+        <form role="search" style="display:inline; margin-left:10px; margin-right:10px;">
+<input class="form-control me-2" style="max-width:300px;display:inline;" type="search" placeholder="Search" aria-label="Search">
+<button class="btn btn-outline-success" type="submit">Search</button>
+</form>
         <div class="float-end">
           <span style="margin-right:10px;">${page} / ${page_total}</span>
           <a href="${page==1?'#':'/?page='+(page-1).toString()}"><button type="button" class="btn btn-dark" ${page==1?'disabled':''}><i class="fa fa-arrow-left" aria-hidden="true"></i></button></a>
@@ -91,15 +93,19 @@ app.get('/', (req, res) => {
         </tbody>
       </table>
       <div style="width:100%; background-color:white; padding:15px; border:5px solid black; border-radius:15px;">
+        <h3>2024.05.15 패치노트</h3>
+        <ul>
+          <li>로그인/회원가입 기능 완성
+        </ul>
+        <hr/>
         <h3>2024.04.23 패치노트</h3>
         <ul>
           <li>DHOUTSIDE 로고 추가(수제작)</li>
           <li>줄(line) 설정 기능 추가</li>
           <li>페이지 넘기기 기능 추가</li>
           <li>수정 및 삭제는 작성자만 가능하도록 변경</li>
-          <li>로그인/회원가입 기능 / 제작 중</li>
-          <li>댓글 기능 / 제작 중</li>
         </ul>
+        <img src="/LJM.png">
       </div>
     `
     script = `
@@ -121,7 +127,7 @@ app.get('/', (req, res) => {
       });
     })
     `
-    res.send(pages.main(html,script));
+    res.send(pages.main(html,script, authStatus(req, res)));
   });
 });
 
@@ -148,7 +154,7 @@ app.get('/view', (req, res) => {
           <a href="#"><button class="btn btn-sm btn-primary">댓글 저장</button></a>
         </div>
       `;
-      res.send(pages.main(html));
+      res.send(pages.main(html,'',authStatus(req, res)));
     } else {
       res.status(404).send("요청하신 게시글을 찾을 수 없습니다람쥐썬더");
     }
@@ -205,11 +211,19 @@ app.get('/edit', (req, res) => {
         html = '<h3>당신은 수정 권한이 없습니다.</h3>'
         script = '';
       }
-      res.send(pages.main(html, script));
+      res.send(pages.main(html, script, authStatus(req, res)));
     });
   } else {
+    // let user;
+    // if(authStatus(req.session.is_logined)){
+    //   user = authStatus(req.session.is_logined);
+    // } else {
+    //   user = req.ip;
+    //   user = user.replace("::ffff:","");
+    // }
     let user = req.ip;
     user = user.replace("::ffff:","");
+    
     const html = `
         <input class="form-control subject" placeholder="제목을 입력하세요" style="margin-bottom:10px;">
         <textarea class="form-control content" style="height:80%;"></textarea>
@@ -248,38 +262,31 @@ app.get('/edit', (req, res) => {
           });
         });
         `;
-    res.send(pages.main(html, script));
+    res.send(pages.main(html, script, authStatus(req, res)));
   }
 });
 
 app.post('/write', (req, res) => {
-  let body = '';
-  req.on('data', function(data) {
-    body = body + data;
-  });
-  req.on('end', function() {
-    const post = qs.parse(body);
-    if (post.status == 'create') {
-      db.query(`INSERT INTO tbl_board (subject, content, user) VALUES (?,?,?);`, [post.subject, post.content, post.user], function(error, data) {
-        if (error) {
-          res.status(500).send('Internal Server Error');
-          return;
-        }
-        console.log(data.insertId);
-        res.send({ insertId: data.insertId });
-      });
-    } else if (post.status == 'update') {
-      db.query(`UPDATE tbl_board SET subject=?, content=?, user=? where id=?`, [post.subject, post.content, post.user, post.id], function(error, data) {
-        if (error) {
-          res.status(500).send('Internal Server Error');
-          return;
-        }
-        res.send();
-      });
-    } else {
-      res.status(404).send('Not found, Go home');
-    }
-  });
+  let post = req.body;
+  if (post.status == 'create') {
+    db.query(`INSERT INTO tbl_board (subject, content, user) VALUES (?,?,?);`, [post.subject, post.content, post.user], function(error, data) {
+      if (error) {
+        res.status(500).send('Internal Server Error');
+        return;
+      }
+      res.send({ insertId: data.insertId });
+    });
+  } else if (post.status == 'update') {
+    db.query(`UPDATE tbl_board SET subject=?, content=?, user=? where id=?`, [post.subject, post.content, post.user, post.id], function(error, data) {
+      if (error) {
+        res.status(500).send('Internal Server Error');
+        return;
+      }
+      res.send();
+    });
+  } else {
+    res.status(404).send('Not found, Go home');
+  }
 });
 
 app.get('/delete', (req, res) => {
@@ -302,15 +309,18 @@ app.get('/delete', (req, res) => {
 });
 
 app.post('/cookie', (req, res) => {
-  let body = '';
-  req.on('data', function(data) {
-    body = body + data;
-  });
-  req.on('end', function() {
-    const post = qs.parse(body);
-    res.cookie('line', post.line, { maxAge: 86400000 }); // 1일 동안 쿠키 유지
-    res.send();
-  });
+  let post = req.body;
+  res.cookie('line', post.line, { maxAge: 86400000 }); // 1일 동안 쿠키 유지
+  res.send();
+});
+
+app.get('/logout', (req, res) => {
+  req.session.destroy((err)=>{
+    if(err)
+      throw err;
+
+    res.redirect('/')
+  })
 });
 
 app.get('/login', (req, res) => {
@@ -356,7 +366,7 @@ app.get('/login', (req, res) => {
     </style>
     <div class="container">
       <h2>로그인</h2>
-      <form action="/login_process" method="POST">
+      <form id="loginForm" action="/login_process" method="POST">
           <div class="form-group">
               <label for="username">사용자 아이디</label>
               <input type="text" id="username" name="username" required>
@@ -371,7 +381,58 @@ app.get('/login', (req, res) => {
       <a href="/signup">혹시 아이디가 없으신가요? 회원가입을 해라 당장.</a>
     </div>
   `
-  res.send(pages.main(html));
+  script = `
+  $(document).ready(function() {
+    $('#loginForm').on('submit', function(event) {
+      event.preventDefault(); // 폼의 기본 제출 동작을 막음
+
+      $.ajax({
+        type: 'POST',
+        url: '/login_process',
+        data: {
+          username: $("#username").val(),
+          password: $("#password").val()
+        },
+        success: function(data) {
+          console.log(data)
+          if (data.success) {
+            window.location.href = '/';
+          } else {
+            alert(data.message);
+            window.location.href = '/login';
+          }
+        },
+        error: function() {
+          console.error('Error occurred');
+          alert('서버 오류가 발생했습니다. 다시 시도해주세요.');
+          window.location.href = '/login';
+        }
+      });
+    });
+  });
+  `
+  res.send(pages.main(html, script, authStatus(req, res)));
+});
+
+app.post('/login_process', (req, res) => {
+  let post = req.body;
+  db.query(`SELECT username, password FROM tbl_user WHERE username=?`,[post.username], function(error, data) {
+    if(error){ //쿼리 자체에 문제가 생겼을 경우
+      return;
+    } else if(data && data.length > 0){ //쿼리 결과가 1개 이상 있을 경우
+      if(data[0].username == post.username && data[0].password == post.password){
+        req.session.is_logined = true;
+        req.session.username = data[0].username;
+        req.session.save( function() {
+          res.json({ success: true });
+        })
+      } else {
+        res.json({ success: false, message: '아이디 또는 비밀번호가 맞지 않습니다.' });
+      }
+    } else { //쿼리 결과가 0개인 경우[ 아이디가 없는 경우. ]
+      res.json({ success: false, message: '아이디 또는 비밀번호가 맞지 않습니다.' });
+    }
+  })
 });
 
 app.get('/signup', (req, res) => {
@@ -434,7 +495,18 @@ app.get('/signup', (req, res) => {
       <br>
     </div>
   `
-  res.send(pages.main(html));
+  res.send(pages.main(html,'',authStatus(req, res)));
+});
+
+app.post('/signup_process', (req, res) => {
+  let post = req.body;
+  db.query(`INSERT INTO tbl_user (username, password) VALUES (?,?);`, [post.username, post.password], function(error, data) {
+    if (error) {
+      res.status(500).send('이미 존재하는 아이디 입니다.');
+      return;
+    }
+    res.redirect('/login');
+  });
 });
 
 app.listen(3000);
